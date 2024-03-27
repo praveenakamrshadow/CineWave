efa4eecbb3d8af2f169c818bd04c77872da5f723
praveen Sahu <praveen.pkyt327867@gmail.com>
2024-03-27 22:40:39 +0530

    added remix icon package

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div className="w-screen h-screen">
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
